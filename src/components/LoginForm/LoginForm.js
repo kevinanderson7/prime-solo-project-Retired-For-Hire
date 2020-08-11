110feac56374278d9f6a5d1c0b2930010863c90b
Kevin Anderson <kmichael419@hotmail.com>
2020-08-11 11:33:42 -0500

    adding Material UI grid to LoginForm to correct spacing

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
 //MaterialUI
-import { Button, TextField, Typography } from '@material-ui/core';
+import { Button, TextField, Typography, Grid } from '@material-ui/core';
 
 class LoginForm extends Component {
   state = {
@@ -36,48 +36,56 @@ class LoginForm extends Component {
   render() {
     return (
       <form className="formPanel" onSubmit={this.login}>
-        <Typography gutterBottom variant="h4" component="h2">
-          Log in
-        </Typography>
-        {this.props.store.errors.loginMessage && (
-          <h3 className="alert" role="alert">
-            {this.props.store.errors.loginMessage}
-          </h3>
-        )}
-        <div>
-          <TextField
-            variant="outlined"
-            label="Username"
-            type="text"
-            name="username"
-            required
-            value={this.state.username}
-            onChange={this.handleInputChangeFor('username')}
-          />
-        </div>
-        <div>
-          <TextField
-            variant="outlined"
-            label="Password"
-            type="password"
-            name="password"
-            required
-            value={this.state.password}
-            onChange={this.handleInputChangeFor('password')}
-          />
-        </div>
-        <div>
-          <Button
-            variant="contained"
-            color="primary"
-            className="log-in"
-            type="submit"
-            name="submit"
-            value="Log In"
-          >
-            Log In
-          </Button>
-        </div>
+        <Grid
+          container
+          alignItems="flex-start"
+          justify="flex-start"
+          direction="column"
+          spacing={1}
+        >
+          <Typography gutterBottom variant="h4" component="h2">
+            Log in
+          </Typography>
+          {this.props.store.errors.loginMessage && (
+            <h3 className="alert" role="alert">
+              {this.props.store.errors.loginMessage}
+            </h3>
+          )}
+          <Grid item>
+            <TextField
+              variant="outlined"
+              label="Username"
+              type="text"
+              name="username"
+              required
+              value={this.state.username}
+              onChange={this.handleInputChangeFor('username')}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              variant="outlined"
+              label="Password"
+              type="password"
+              name="password"
+              required
+              value={this.state.password}
+              onChange={this.handleInputChangeFor('password')}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              color="primary"
+              className="log-in"
+              type="submit"
+              name="submit"
+              value="Log In"
+            >
+              Log In
+            </Button>
+          </Grid>
+        </Grid>
       </form>
     );
   }
