019f1ee723294ed2562b4dc878efbebff9b6a7b7
Kevin Anderson <kmichael419@hotmail.com>
2020-09-08 11:54:31 -0500

    fixing url for server on amazon s3

--- src/components/ImageUpload/ImageUpload.js
+++ src/components/ImageUpload/ImageUpload.js
@@ -5,7 +5,7 @@ import DropzoneS3Uploader from 'react-dropzone-s3-uploader';
 class ImageUpload extends Component {
   render() {
     const uploadOptions = {
-      server: 'https://retired-for-hire.herokuapp.com/',
+      server: 'https://retired-for-hire.herokuapp.com',
       //server: localhost:5000 to run locally instead of heroku
       signingUrlQueryParams: { uploadType: 'avatar' },
     };
