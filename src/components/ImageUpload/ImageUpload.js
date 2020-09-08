3539298cc4a26f0d60235602da91209698868ab7
Kevin Anderson <kmichael419@hotmail.com>
2020-09-08 11:45:15 -0500

    updating image upload server location for heroku use in image upload component

--- src/components/ImageUpload/ImageUpload.js
+++ src/components/ImageUpload/ImageUpload.js
@@ -5,7 +5,8 @@ import DropzoneS3Uploader from 'react-dropzone-s3-uploader';
 class ImageUpload extends Component {
   render() {
     const uploadOptions = {
-      server: 'http://localhost:5000',
+      server: 'https://retired-for-hire.herokuapp.com/#/home',
+      //server: localhost:5000 to run locally instead of heroku
       signingUrlQueryParams: { uploadType: 'avatar' },
     };
     const s3Url = 'https://retired-for-hire.s3.amazonaws.com';
