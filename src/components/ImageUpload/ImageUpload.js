8f9659b7aeea6f07624ade2a3028f62e572a4059
Kevin Anderson <kmichael419@hotmail.com>
2020-09-11 18:43:27 -0500

    updating styling on s3 uploader

--- src/components/ImageUpload/ImageUpload.js
+++ src/components/ImageUpload/ImageUpload.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import DropzoneS3Uploader from 'react-dropzone-s3-uploader';
+import Dropzone from 'react-dropzone-s3-uploader';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import './ImageUpload.css';
+const dropStyles = {
+  width: '200px',
+  height: '50px',
+  border: '1px solid black',
+  backgroundColor: 'whitesmoke',
+  color: 'green',
+};
 
 class ImageUpload extends Component {
   handleFinishedUpload = (info) => {
@@ -22,20 +30,69 @@ class ImageUpload extends Component {
     });
   };
   render() {
-    const uploadOptions = {
-      server: 'https://retired-for-hire.herokuapp.com',
-      // server: 'http://localhost:5000',
-      //server: localhost:5000 to run locally instead of heroku
-      signingUrlQueryParams: { uploadType: 'avatar' },
-    };
+    // const uploadOptions = {
+    //   server: 'https://retired-for-hire.herokuapp.com',
+    //   // server: 'http://localhost:5000',
+    //   //server: localhost:5000 to run locally instead of heroku
+    //   signingUrlQueryParams: { uploadType: 'avatar' },
+    // };
     const s3Url = 'https://retired-for-hire.s3.amazonaws.com';
+
+    const innerDropElement = (
+      <div className="inner-drop">
+        <p>Click or Select a File Here</p>
+      </div>
+    );
+
+    const toast = (innerHTML) => {
+      const el = document.getElementById('toast');
+      el.innerHTML = innerHTML;
+      el.className = 'show';
+      setTimeout(() => {
+        el.className = el.className.replace('show', '');
+      }, 3000);
+    };
+
+    const getUploadParams = () => {
+      return { url: 'https://retired-for-hire.herokuapp.com' };
+    };
+
+    const handleChangeStatus = ({ meta, remove }, status) => {
+      if (status === 'headers_received') {
+        toast(`${meta.name} uploaded!`);
+        remove();
+      } else if (status === 'aborted') {
+        toast(`${meta.name}, upload failed...`);
+      }
+    };
     return (
-      <DropzoneS3Uploader
-        onFinish={this.handleFinishedUpload}
-        s3Url={s3Url}
-        maxSize={1024 * 1024 * 5}
-        upload={uploadOptions}
-      />
+      <React.Fragment>
+        <div id="toast">Upload an Image Here</div>
+        <Dropzone
+          getUploadParams={getUploadParams}
+          onChangeStatus={handleChangeStatus}
+          // upload={uploadOptions}
+          s3Url={s3Url}
+          maxFiles={1}
+          multiple={false}
+          canCancel={false}
+          inputContent="Drop A File"
+          styles={{
+            dropzone: { width: 400, height: 200 },
+            dropzoneActive: { borderColor: 'green' },
+          }}
+        />
+      </React.Fragment>
+      // <DropzoneS3Uploader
+      //   getUploadParams={getUploadParams}
+      //   onChangeStatus={handleChangeStatus}
+      //   children={innerDropElement}
+      //   onFinish={this.handleFinishedUpload}
+      //   s3Url={s3Url}
+      //   // style={dropStyles}
+      //   maxSize={1024 * 1024 * 5}
+      //   upload={uploadOptions}
+      // />
     );
   }
 }
